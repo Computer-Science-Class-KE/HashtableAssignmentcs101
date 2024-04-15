ba2f429344bc2ec4d888a6221b1e379b4074d0e5
Margaret Sitati (WTS International Limited) <v-masitati@microsoft.com>
2024-04-15 09:22:31 +0300

    Add student boiler plate template

---
+++ HashtableClass/HashtableClass/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashtableClass
+{
+    internal class Solution
+    {
+        public Solution()
+        {
+
+        }
+
+        public void Put(int key, int value)
+        {
+
+        }
+
+        public int Get(int key)
+        {
+
+        }
+
+        public void Remove(int key)
+        {
+
+        }
+    }
+}
